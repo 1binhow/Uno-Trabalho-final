5ed518e9e2a2be6d3bc56c77eb392e112bd3fb70
1binhow <89793470+1binhow@users.noreply.github.com>
2025-01-06 18:31:03 -0300

    Update atualiza_rodada.h

--- atualiza_rodada.h
+++ atualiza_rodada.h
@@ -4,10 +4,13 @@
 #include "TADs.h"
 
 
-void atualizaRodada(PilhaCartas* pilha, PilhaCartas* baralho, Jogador* jogador);
-void compraCarta(CartasPilha* baralho, Mao* mao, int n);
-void inputsJogador(Mao* mao, PilhaCartas* pilha, PilhaCartas* baralho);
+void compraCarta(PilhaCartas* baralho, Mao* mao, int n);
+void inputsJogador(Mao* mao, PilhaCartas* pilha, PilhaCartas* baralho, int* cartaJogada, int* pulaVez);
+int verificarCliqueCarta(Vector2 mousePos, Vector2 posicaoCarta);
 void jogadaNPC(Mao* mao, PilhaCartas* pilha, PilhaCartas* baralho, int* cartaJogada, int* pulaVez);
 int jogadaValida(Carta* carta, PilhaCartas* pilha);
+void alteraCor(CartaPilha* carta);
+void atualizaRodada(PilhaCartas* pilha, PilhaCartas* baralho, Jogador** jogador, ListaJogadores* ls_jogadores, int* inversao, int* cartaJogada, int* winner, int* uno);
 
 #endif
+
